8e602be25b102c5f7ad345f90ea6cfa85b205c9e
[Keval Patel] <[khp90611@gmail.com]>
2023-10-04 11:27:20 -0400

    Added css for profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -9,7 +9,22 @@ const Profile = () => {
 
 
     return (
-       
+        <main>
+        <div id="profile">
+            <img id='profile-pic' src="./assets/Profile-Avatar.png" alt="Generic Profile Image"/>
+            <h1 id="username">Username</h1>
+        </div>
+        <div id="recipe-section">
+            <div id="create-recipe">
+                <img id='profile-pic' src="./assets/Create-Ingredient.png" alt="Generic Profile Image"/>
+                <h1 class="recipe-header">Create new Recipe</h1>
+            </div>
+            <div id="saved-recipes">
+                <img id='profile-pic' src="./assets/Recipes.png" alt="Generic Profile Image"/>
+                <h1 class="recipe-header">Saved Recipes</h1>
+            </div>
+        </div>
+    </main>
     )
 
 
