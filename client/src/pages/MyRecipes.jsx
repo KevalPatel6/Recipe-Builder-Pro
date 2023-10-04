a891021a0b2b2cd70ea72a0fae09ea089f63372c
[Keval Patel] <[khp90611@gmail.com]>
2023-10-04 14:48:31 -0400

    Previous commit was this commits work-  Added basic logic to load saved and created recipes and render the appropriate component. Need to workout the If statement and test after back end is done. Created a MyRecipe folder in components and added CreatedRecipes and SavedRecipes as jsx files. Adjusted class to be className for all. Added html for both Created Recipes and Saved Recipes. Need to adjust this

--- client/src/pages/MyRecipes.jsx
+++ client/src/pages/MyRecipes.jsx
@@ -1,41 +1,49 @@
+import { QUERY_ME } from "../utils/queries"
+import { useParams } from "react-router"
 
 
 const MyRecipes = () => {
+    const { profileId } = useParams()
+    const { loading, data } = useQuery(QUERY_ME, {
+        variables: { _id: profileId }
+    })
 
-
-
+    if (loading) {
+        return <div>
+            <img src="../assets/spinner.gif" alt="Spinning Loading Symbol" />
+        </div>
+    }
 
 
     return (
         <main>
             <div>
-                <h1 class="title-headers">My Recipes</h1>
+                <h1 className="title-headers">My Recipes</h1>
             </div>
-            <div class="recipes-container">
-
-        {/* NEED TO MAP USERS MY RECIPES ARRAY TO THE CARD BELOW */}
-
-                <div class="recipe-card">
-                    {/* <!-- Recipe Image --> */}
-                    <div id="image-container">
-                        <img class="recipe-image" src="./assets/generic-meal.jpg" alt="Recipe Image" />
-                        <button class="save-icon btn">
-                            <svg class='save-icon' fill="#000000" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M14 20.408c-.492.308-.903.546-1.192.709-.153.086-.308.17-.463.252h-.002a.75.75 0 01-.686 0 16.709 16.709 0 01-.465-.252 31.147 31.147 0 01-4.803-3.34C3.8 15.572 1 12.331 1 8.513 1 5.052 3.829 2.5 6.736 2.5 9.03 2.5 10.881 3.726 12 5.605 13.12 3.726 14.97 2.5 17.264 2.5 20.17 2.5 23 5.052 23 8.514c0 3.818-2.801 7.06-5.389 9.262A31.146 31.146 0 0114 20.408z" /></svg>
-                        </button>
+            <div className="recipes-container">
+                {/* If there is user recipes*/}
+                {data.me.createRecipes.length ?
+                    <CreatedRecipes recipes={data.me.createdRecipes}/>
+                    :
+                    <div>
+                        <h3>You have not created any recipes!</h3>
                     </div>
-                    {/* <!-- Recipe Content --> */}
-                    <div class="recipe-content">
-                        {/* <!-- Recipe Title --> */}
-                        <h2 class="recipe-title">Title of Recipe</h2>
-
-                        {/* <!-- Recipe Description --> */}
-                        <p class="recipe-description">A mouthwatering and easy-to-make recipe that you'll love.</p>
-
-                        {/* <!-- Link to Recipe --> */}
-                        <a class="recipe-link" href="">View More</a>
-                    </div>
-                </div>
+                }
             </div>
+            <div>
+                <h1 className="title-headers">Saved Recipes</h1>
+            </div>
+            <div className="recipes-container">
+                {/* If there is user recipes*/}
+                {data.me.savedRecipes.length ?
+                    <SavedRecipes recipes={data.me.savedRecipes}/>
+                    :
+                    <div>
+                        <h3>You have not saved any recipes!</h3>
+                    </div>
+                }
+            </div>
+
         </main>
     )
 
