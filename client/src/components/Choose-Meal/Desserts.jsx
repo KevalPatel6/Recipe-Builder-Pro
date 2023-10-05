91d5d75a799d0cc18f218749f4005759b41d26af
jtdoaks <136029371+jtdoaks@users.noreply.github.com>
2023-10-05 14:09:32 -0400

    drinks, meals, desserts components started

---
+++ client/src/components/Choose-Meal/Desserts.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
+import { useQuery } from '@apollo/client';
+import { QUERY_RECIPE_GROUP } from '../../utils/queries';
+
+import '../../styles/Login.css'
+
+import Auth from '../../utils/auth';
+
+function SeeDesserts() {
+
+    return (
+        <div>
+            <h1>Desserts</h1>
+            <button>
+                <Link to="/desserts-page"></Link>
+            </button>
+
+        </div>
+    )
+}
+
+
+
+export default SeeDesserts;
