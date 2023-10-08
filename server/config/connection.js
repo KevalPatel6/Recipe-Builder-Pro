7fc57de6964a1b0831cd3c98eea1453e6ab7742b
joceamayar <jocelin@tavlee.com>
2023-10-08 01:51:40 -0400

    Fix Heroku attempt

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/recipe-builder');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/recipe-builder');
 
 //if enviroment vairable mangodb ui exists use this as the connnection string o9ther wise use the second connection string 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://pantryPal:pantryPal@pantrypal.lradei2.mongodb.net/?retryWrites=true&w=majority');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://pantryPal:pantryPal@pantrypal.lradei2.mongodb.net/?retryWrites=true&w=majority');
 
 module.exports = mongoose.connection;
