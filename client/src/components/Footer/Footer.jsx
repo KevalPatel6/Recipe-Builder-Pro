d7da051db8fd1438c37964c211ee11f838c8dbfb
cpichac <Carlos12pichardo@outlook.com>
2023-10-09 09:52:30 -0400

    save files

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,11 +1,26 @@
-
+import React from 'react';
 
 function Footer() {
     return (
-        <div className="footer-container">
-
-        </div>
-
+                <footer className="footer">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-6">
+                                <p>&copy; 2023 Recipe Pro Builder</p>
+                            </div>
+                            <div className="col-md-6 text-md-right">
+                                <a
+                                    href="https://github.com/KevalPatel6/Recipe-Builder-Pro"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <img src="github-badge.png" alt="GitHub" className="github-badge" />
+                                </a>
+                                <p className="mt-2">MIT License</p>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
     )
 }
 
