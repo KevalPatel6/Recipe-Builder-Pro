6a90dfab26a12f489add1594bf5e8a81b53ad2e6
joceamayar <jocelin@tavlee.com>
2023-10-09 20:26:54 -0400

    home page is done, as well as navigation bar and footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,8 +4,8 @@ import './Footer.css'; // Import the CSS file
 function Footer() {
   return (
     <footer className="footer">
-      <div className="container">
-        <p>&copy; 2023 Recipe Pro Builder</p>
+      <div className="footer-container">
+        <small>&copy; 2023 Recipe Pro Builder</small>
         <div className="footer-links">
           <a href="https://github.com/KevalPatel6/Recipe-Builder-Pro">GitHub</a>
           <a href="/privacy-policy">Privacy Policy</a>
