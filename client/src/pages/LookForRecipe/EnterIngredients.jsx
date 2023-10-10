cfd274df754f4d8d1a444aedc2ab1e9a3f606a32
[Keval Patel] <[khp90611@gmail.com]>
2023-10-10 06:15:17 -0400

    Fixed this

--- client/src/pages/LookForRecipe/EnterIngredients.jsx
+++ client/src/pages/LookForRecipe/EnterIngredients.jsx
@@ -3,72 +3,100 @@ import { useNavigate } from 'react-router-dom'
 import { useMutation, useQuery } from '@apollo/client';
 import { ADD_INGREDIENT_TO_USER } from '../../utils/mutations';
 import { QUERY_ALL_RECIPES, QUERY_ME, GET_FILTERED_RECIPES } from '../../utils/queries';
+import _ from 'lodash';
 
 import Auth from '../../utils/auth';
+import AllRecipes from '../AllRecipes';
 // import './EnterIngredients.css';
 
 function Pantry() {
-
+  //State Variables//
   const [ingName, setIngName] = useState('');
   const [ingredients, setIngredients] = useState([]);
   const [filteredRecipes, setFilteredRecipes] = useState([]);
-
+  //Mutations//
   const [addIngredient] = useMutation(ADD_INGREDIENT_TO_USER);
+  //Queries//
+  const { data: userData } = useQuery(QUERY_ME)
+  const { loading, error, data, refetch } = useQuery(QUERY_ALL_RECIPES);
 
-  const {data: userData} = useQuery(QUERY_ME)
-  //Want array of userRecipes
-    let userIngredients = userData?.me?.ingredients
-    console.log(userIngredients)
+  //Variables used in Matching//
+  const userIngredients = userData?.me?.ingredients || []
+  const matchedIngredients = [];
+  let recipes = data?.getAllRecipes || []  
+  let matchedRecipes = []
 
-  
-  const { loading, error, data, refetch } = useQuery(QUERY_ALL_RECIPES)  ;
+  //Matches User ingredients with Recipes//
+  const handleMatchRecipe = async (event) => {
+    event.preventDefault()
 
-  //data.getAllRecipes is array of all recipes
-  let allRecipes = data?.getAllRecipes
-  let eachRecipesIngredients = data?.getAllRecipes.map(recipe=>recipe.ingredients)
+    const eachRecipesIngredients = await data?.getAllRecipes.map((recipe) => recipe.ingredients)
+   
 
-  const flattenedIngredients = eachRecipesIngredients.flat()
-  // const recipes = data?.recipes || [];
-  // const user = data?.getFilteredRecipes.user || {};
+    findMatchingIngredients(eachRecipesIngredients, userIngredients)
+   
+    matchedRecipes = await matchedIngredients.map((index)=>recipes[index.arrayIndex])
+    console.log(matchedRecipes)
 
-  // console.log(user)
-  // console.log(data?.getFilteredRecipes)
-  
+    setFilteredRecipes(_.uniqBy(matchedRecipes,'title'))
+
+  }
+
+  //Matches Ingredients of recipes and User Ingredients//
+  async function findMatchingIngredients(eachRecipesIngredients, userIngredients) {
+    for (let i = 0; i < eachRecipesIngredients.length; i++) {
+      const currentArray = eachRecipesIngredients[i];
+      for (let j = 0; j < currentArray.length; j++) {
+        const currentObject = currentArray[j].name;
+
+        const isMatch = userIngredients.some((targetObject) =>
+          isEqual(currentObject, targetObject.name)
+        );
+        if (isMatch) {
+          matchedIngredients.push({object: currentObject, arrayIndex: i});
+        }
+      }
+    }
+    return matchedIngredients;
+
+  }
+
+  //Minor function used in function findMatchingIngredients to see if two objects are equal//
+  function isEqual(objA, objB){
+    if(objA==objB){
+      return true
+    }
+    else{
+      return false
+    }
+  }
+
   const handleClick = async (event) => {
     event.preventDefault();
 
     try {
-  
+
       const { data } = await addIngredient({
         variables: { name: ingName },
       });
 
       refetch();
-      setIngredients([...ingredients, { name: ingName }]);
-      
+      setIngredients([...userIngredients, { name: ingName }]);
+
       setIngName('');
-     
+
     } catch (err) {
       console.error(err);
     }
   };
 
 
-  const handleGetRecipesClick = async (event) => {
-    event.preventDefault();
-    try {
-      // console.log(data.getAllRecipes)
-      const filtered = data.getAllRecipes.filter((recipe) => 
-        recipe.ingredients.some((ingredient) => user.ingredients.includes(ingredient.name))
-      );
-      setFilteredRecipes(filtered);
-      
-    } catch (error) {
+  useEffect(()=>{
+    console.log(filteredRecipes);
+    console.log(ingredients)
+  },[filteredRecipes], [ingredients])
 
-    }
-  };
 
- 
   return (
     <div className="header-container">
       <div id="searchContainer" className="container">
@@ -81,7 +109,7 @@ function Pantry() {
 
 
           <button onClick={handleClick} >Add</button>
-          <button onClick={handleGetRecipesClick} >Get Recipes</button>
+          <button onClick={handleMatchRecipe} >Get Recipes</button>
 
         </div>
 
@@ -91,23 +119,27 @@ function Pantry() {
         <div id="ingredientList">
           <h2>Ingredients:</h2>
           <ul>
-            {ingredients?.map((ingredient, index) => (
-              <li key={index}>
+            {userIngredients?.length ? (
+              userIngredients.map((ingredient, index) => (
+
+                <li style={{color: 'red'}} key={index}>
                 {ingredient.name} - {ingredient.group}
               </li>
-            ))}
+                ))
+            ) : ("")
+            }
           </ul>
         </div>
       </div>
 
       <h2>Recipes with Specific Ingredients</h2>
       <ul>
-        {recipes.length ? (
-          recipes.map((recipe) => (
-            <li key={recipe._id}>{recipe.title}</li>
+        {filteredRecipes?.length ? (
+          filteredRecipes.map((recipe) => (
+            <li style={{color: 'red'}} key={recipe._id}>{recipe.title }</li>
           ))
         ) : ("")
-      }
+        }
       </ul>
     </div>
 
